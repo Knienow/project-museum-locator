6c825bcaa0b92a702fc9a141c1759f677c277c38
Katia <nienowkatia@gmail.com>
2023-11-13 19:13:47 -0300

    criei a classe de serviço e implementei o método createMuseum, que vem da interface

---
+++ src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
@@ -0,0 +1,8 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * Lança uma exceção caso as coordenadas não sejam válidas.
+*/
+public class InvalidCoordinateException extends RuntimeException {
+    
+}
---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,45 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+/**
+ * Classe MuseumService.
+*/
+
+@Service
+public class MuseumService implements MuseumServiceInterface {
+  MuseumFakeDatabase museumFakeDatabase;
+
+  @Autowired
+  public MuseumService(MuseumFakeDatabase museumFakeDatabase) {
+    this.museumFakeDatabase = museumFakeDatabase;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    throw new UnsupportedOperationException("Unimplemented method 'getClosestMuseum'");
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    if (!CoordinateUtil.isCoordinateValid(museum.getCoordinate())) {
+      throw new InvalidCoordinateException();
+    }
+    return museumFakeDatabase.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    throw new UnsupportedOperationException("Unimplemented method 'getMuseum'");
+  }
+    
+}
+ 
