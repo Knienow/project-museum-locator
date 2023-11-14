8046de0324f941697e918508c1c71480f2649aa8
Katia <nienowkatia@gmail.com>
2023-11-14 09:39:03 -0300

    requisito 04 - ok

---
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -0,0 +1,9 @@
+package com.betrybe.museumfinder.exception;
+
+
+/**
+ * Lançará uma exceção caso nenhum museu seja encontrado.
+*/
+public class MuseumNotFoundException extends RuntimeException {
+    
+}
